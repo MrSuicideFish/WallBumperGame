d9a34881021c9cd0b041ceb957e8a49a89a043c7
Malcolm Neal <Malcolm259@Gmail.com>
2016-01-03 01:07:31 -0500

    screen debug info

--- Assets/Scripts/InputController.cs
+++ Assets/Scripts/InputController.cs
@@ -13,6 +13,12 @@ public class InputController : MonoBehaviour
         TiltVector = Vector3.zero;
     }
 
+    float deltaTime = 0.0f;
+    void Update( )
+    {
+        deltaTime += ( Time.deltaTime - deltaTime ) * 0.1f;
+    }
+
     void FixedUpdate( )
     {
 #if UNITY_EDITOR && UNITY_ANDROID
@@ -30,5 +36,26 @@ public class InputController : MonoBehaviour
         m_Player.AddTorque( TiltVector * m_MovementSpeed );
 
         TiltVector = Vector3.zero;
+    }
+
+    void OnGUI( )
+    {
+        GUI.Label( new Rect( 0, 20, 256, 32 ), "Gyro Enabled: " + Input.gyro.enabled );
+        GUI.Label( new Rect( 0, 40, 256, 32 ), "Gyro Attitude: " + Input.gyro.attitude.eulerAngles.ToString( ) );
+        GUI.Label( new Rect( 0, 60, 256, 32 ), "Rotation Rate: " + Input.gyro.rotationRate.ToString( ) );
+        GUI.Label( new Rect( 0, 80, 256, 32 ), "Rotation Rate (Unbiased): " + Input.gyro.rotationRateUnbiased.ToString( ) );
+
+        int w = Screen.width, h = Screen.height;
+
+        GUIStyle style = new GUIStyle( );
+
+        Rect rect = new Rect( 0, 0, w, h * 2 / 100 );
+        style.alignment = TextAnchor.UpperLeft;
+        style.fontSize = h * 2 / 100;
+        style.normal.textColor = new Color( 1.0f, 1.0f, 1.0f, 1.0f );
+        float msec = deltaTime * 1000.0f;
+        float fps = 1.0f / deltaTime;
+        string text = string.Format( "{0:0.0} ms ({1:0.} fps)", msec, fps );
+        GUI.Label( rect, text, style );
     }
 }
