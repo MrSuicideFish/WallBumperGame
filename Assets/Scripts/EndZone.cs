deb6eca7ffc598dff64a8a4c848db08be6a75925
Malcolm Neal <Malcolm259@Gmail.com>
2016-01-04 20:40:40 -0500

    added finish line with two types

--- Assets/Scripts/EndZone.cs
+++ Assets/Scripts/EndZone.cs
@@ -1,15 +1,37 @@
 ﻿using UnityEngine;
+using UnityEditor;
 using System.Collections;
 
-public class EndZone : MonoBehaviour {
+public enum EndZoneShape
+{
+    Cube,
+    Sphere
+}
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+public class EndZone : MonoBehaviour
+{
+    string m_MeshPath = "Gizmos/Gizmo_FinishLine.obj";
+
+    public EndZoneShape EndZoneType = EndZoneShape.Cube;
+    public float ZoneSize = 1.0f;
+
+    void OnDrawGizmos( )
+    {
+        if ( ZoneSize < 0 ) ZoneSize = 0;
+
+        Gizmos.color = Color.red;
+        Mesh gizmo = AssetDatabase.LoadAssetAtPath<Mesh>( "Assets/Editor/Gizmo_FinishLine.obj" );
+        Gizmos.DrawMesh( gizmo, transform.position );
+
+        Gizmos.color = Color.magenta;
+        switch ( EndZoneType )
+        {
+            case EndZoneShape.Cube:
+                Gizmos.DrawWireCube( transform.position, new Vector3( ZoneSize, 0.3f, ZoneSize ) );
+                break;
+            case EndZoneShape.Sphere:
+                Gizmos.DrawWireSphere( transform.position, ZoneSize );
+                break;
+        }
+    }
 }
