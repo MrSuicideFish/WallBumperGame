88865bc787270f35c6844f72f02e869ceb13841b
Malcolm Neal <Malcolm259@Gmail.com>
2016-01-04 23:25:29 -0500

    added checkpoints

---
+++ Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEditor;
+using System.Collections;
+
+public class Checkpoint : MonoBehaviour
+{
+    string m_MeshPath = "Gizmos/Gizmo_FinishLine.obj";
+
+    public EndZoneShape CheckpointType = EndZoneShape.Cube;
+    public float ZoneSize = 1.0f;
+
+    void OnDrawGizmos( )
+    {
+        if ( ZoneSize < 0 ) ZoneSize = 0;
+
+        Gizmos.color = Color.yellow;
+        Mesh gizmo = AssetDatabase.LoadAssetAtPath<Mesh>( "Assets/Editor/Gizmo_FinishLine.obj" );
+        Gizmos.DrawMesh( gizmo, transform.position );
+
+        Gizmos.color = Color.magenta;
+        switch ( CheckpointType )
+        {
+            case EndZoneShape.Cube:
+                Gizmos.DrawWireCube( transform.position, new Vector3( ZoneSize, 0.3f, ZoneSize ) );
+                break;
+            case EndZoneShape.Sphere:
+                Gizmos.DrawWireSphere( transform.position, ZoneSize );
+                break;
+        }
+    }
+}
